eee1fa05aa074a336d5040e442b54f217d95807a
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-03-28 22:08:40 +0200

    add: plasma kvantum engine

--- hosts/shared/desktops/plasma.nix
+++ hosts/shared/desktops/plasma.nix
@@ -1,7 +1,8 @@
-{
+{pkgs, ...}: {
   # Enable the KDE Plasma Desktop Environment.
   services.xserver = {
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
   };
+  environment.systemPackages = with pkgs; [kdePackages.qtstyleplugin-kvantum];
 }
