fa182781ac92672ceb837a8632392dd7c7b7c382
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-04-03 02:29:28 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
