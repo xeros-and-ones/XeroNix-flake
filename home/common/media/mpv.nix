483bcdda6c89cd88f2043a52f3d9adab2422d209
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-04-11 02:47:49 +0200

    Chore: flake update

--- home/common/media/mpv.nix
+++ home/common/media/mpv.nix
@@ -2,5 +2,15 @@
   programs.mpv = {
     enable = true;
     scripts = with pkgs.mpvScripts; [mpris uosc thumbfast];
+    config = {
+      force-window = true;
+      geometry = "100%x100%-+0-+0";
+    };
+    bindings = {
+      "UP" = "add volume 5";
+      "DOWN" = "add volume -5";
+      "Ctrl+UP" = "add speed 0.1";
+      "Ctrl+DOWN" = "add speed -0.1";
+    };
   };
 }
