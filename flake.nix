80b0d2e13abda9e6ea2e654c82b282eedcf8c1bf
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-06-19 13:31:58 +0300

    chore: bump up nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -3,13 +3,11 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Shameless plug: looking for a way to nixify your themes and make
     # everything match nicely? Try nix-colors!
