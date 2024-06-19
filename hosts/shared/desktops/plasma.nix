a5026268fadd09edde44eba51a347df188fd0882
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-06-19 12:54:51 +0300

    chore: update changed optins

--- hosts/shared/desktops/plasma.nix
+++ hosts/shared/desktops/plasma.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver = {
+  services = {
+    xserver.desktopManager.plasma5.enable = true;
     displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
   };
   environment.systemPackages = [
     pkgs.libsForQt5.qtstyleplugin-kvantum
