9044e1057efd71f33e23c4161cfd9465983ccd33
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-06-19 09:52:09 +0300

    fix: add usbmuxd service

--- hosts/shared/optional/droidcam.nix
+++ hosts/shared/optional/droidcam.nix
@@ -5,6 +5,8 @@
 }: {
   environment.systemPackages = with pkgs; [v4l-utils droidcam];
 
+  services.usbmuxd.enable = true;
+
   boot = {
     extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
     kernelModules = ["v4l2loopback" "snd-aloop"];
