f56cae92fb5d5ad6ccf65838efe5c55f251625c1
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-06-19 09:34:42 +0300

    feat: fix droidcam

--- hosts/shared/optional/droidcam.nix
+++ hosts/shared/optional/droidcam.nix
@@ -1,3 +1,12 @@
 {
-  programs.droidcam.enable = true;
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = with pkgs; [v4l-utils];
+
+  boot = {
+    extraModulePackages = with config.boot.kernelPackages; [v4l2loopback];
+    kernelModules = ["v4l2loopback" "snd-aloop"];
+  };
 }
