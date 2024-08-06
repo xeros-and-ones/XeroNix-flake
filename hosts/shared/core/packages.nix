96a5c064fea35c885991f1bfab8ca05738a2a03a
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-08-07 02:00:32 +0300

    ......

--- hosts/shared/core/packages.nix
+++ hosts/shared/core/packages.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  system,
+  ...
+}: let
   myEmacs =
     (pkgs.emacsPackagesFor (pkgs.emacs29.override {
       withNativeCompilation = true;
@@ -9,7 +13,7 @@
   pkgsChrome = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/67b4bf1df4ae54d6866d78ccbd1ac7e8a8db8b73.tar.gz";
     sha256 = "07gzgcgaclgand7j99w45r07gc464b5jbpaa3wmv6nzwzdb3v3q4";
-  }) {};
+  }) {inherit system;};
   myChrome = pkgsChrome.google-chrome;
 in {
   environment = {
