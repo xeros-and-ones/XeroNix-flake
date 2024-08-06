5953f4f1d26cdfda40fdd192fe0e779033e92057
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-08-07 01:34:04 +0300

    ...

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,9 @@
 # This file defines overlays
 {inputs, ...}: let
-  chrome-125-src = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/67b4bf1df4ae54d6866d78ccbd1ac7e8a8db8b73.tar.gz");
+  chrome-125-src = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/67b4bf1df4ae54d6866d78ccbd1ac7e8a8db8b73.tar.gz";
+    sha256 = "07gzgcgaclgand7j99w45r07gc464b5jbpaa3wmv6nzwzdb3v3q4";
+  });
 in {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs {pkgs = final;};
