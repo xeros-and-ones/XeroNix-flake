a663a742e8728fd6f31ecc873cdfb0ce2ff97002
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-08-07 09:07:08 +0300

    ....

--- hosts/shared/core/packages.nix
+++ hosts/shared/core/packages.nix
@@ -39,6 +39,7 @@ in {
         .overrideAttrs {
           version = "126.0.6478.126	";
           src = fetchurl {
+            url = "https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_126.0.6478.126	-1_amd64.deb";
             hash = "";
           };
         })
