0f079efbea07fab13c3205b14a4e5fc2edc3902b
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-08-08 01:52:10 +0300

    feat: setup bluetooth support

--- hosts/shared/core/bluetooth.nix
+++ hosts/shared/core/bluetooth.nix
@@ -1,3 +1,13 @@
 {
-  hardware.bluetooth.enable = true;
+  hardware.bluetooth = {
+    enable = true;
+    settings = {
+      General = {
+        Name = "Xero-NixOS-Laptop";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+    };
+  };
 }
