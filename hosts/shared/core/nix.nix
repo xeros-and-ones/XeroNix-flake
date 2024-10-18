def1a0f5afed4493996afc678ea1454c11ed16e2
Mohamed Tarek <mohamed96tarek@hotmail.com>
2024-10-18 05:54:01 +0300

    fix: remove repl-flake option

--- hosts/shared/core/nix.nix
+++ hosts/shared/core/nix.nix
@@ -8,7 +8,7 @@
     settings = {
       trusted-users = ["root" "@wheel"];
       auto-optimise-store = lib.mkDefault true;
-      experimental-features = ["nix-command" "flakes" "repl-flake"];
+      experimental-features = ["nix-command" "flakes"];
       flake-registry = ""; # Disable global flake registry
     };
     gc = {
